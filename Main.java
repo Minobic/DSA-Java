0fe57e11570dcaf6fb465e85accb62d98a26d9d7
Minobic <mayank04varma@gmail.com>
2024-04-16 22:47:33 +0530

    Sudoku solver added - recursion completed

--- Main.java
+++ Main.java
@@ -3179,7 +3179,7 @@ public class Main {
 
 
 // N Queens Problem
-// Time complexitiy -> O(n!)
+// Time complexity -> O(n!)
 
 
 /*
@@ -3339,26 +3339,13 @@ public class Main {
 
 
 // Sudoku Solver
+// Time complexity -> O(9^n^2)
 
 
 /*
-class SudokuSolver {
-    int[][] sudoku = new int[9][9];
-
-    public SudokuSolver(int[][] arr) {
-        sudoku = arr;
-    }
-
-    public void solve(int[][] arr, int row, int col) {
-        if () {
-            
-        }
-    }
-}
-
 public class Main {
     public static void main(String[] args) {
-        int[][] arr = {
+        int[][] board = {
             {3, 0, 6, 5, 0, 8, 4, 0, 0},
             {5, 2, 0, 0, 0, 0, 0, 0, 0},
             {0, 8, 7, 0, 0, 0, 0, 3, 1},
@@ -3369,6 +3356,90 @@ public class Main {
             {0, 0, 0, 0, 0, 0, 0, 7, 4},
             {0, 0, 5, 2, 0, 6, 3, 0, 0}
         };
+
+        if (sudokuSolver(board)) {
+            display(board);
+        } else {
+            System.out.println("Cannot solve");
+        }
+    }
+
+    static boolean sudokuSolver(int[][] board) {
+        int n = board.length;
+        int row = -1;
+        int col = -1;
+
+        boolean emptyLeft = true;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 0) {
+                    row = i;
+                    col = j;
+                    emptyLeft = false;
+                    break;
+                }
+            }
+
+            if (emptyLeft == false) {
+                break;
+            }
+        }
+
+        if (emptyLeft == true) {
+            return true;    // Sudoku is solved
+        }
+
+        for (int number = 1; number <= 9; number++) {
+            if (isSafe(board, row, col, number)) {
+                board[row][col] = number;
+                if (sudokuSolver(board)) {
+                    return true;    // Found the answer
+                } else {
+                    board[row][col] = 0;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private static void display(int[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static boolean isSafe(int[][] board, int row, int col, int num) {
+        // Check for rows
+        for (int i = 0; i < board.length; i++) {
+            if (board[row][col] == num) {
+                return false;
+            }
+        }
+
+        // Check for columns
+        for (int[] nums : board) {
+            if (nums[col] == num) {
+                return false;
+            }
+        }
+
+        int sqrt = (int) Math.sqrt(board.length);
+        int start = row - (row % sqrt);
+        int end = col - (col % sqrt);
+
+        for (int r = start; r < start + sqrt; r++) {
+            for (int c = end; c < end + sqrt; c++) {
+                if (board[r][c] == num) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 } */
 /***************************/
