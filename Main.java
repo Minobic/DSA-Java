de6d538f7165dbed7e866883d77db41ecda81dec
Minobic <mayank04varma@gmail.com>
2024-04-16 18:00:34 +0530

    n queen problem added - recursion

--- Main.java
+++ Main.java
@@ -3175,6 +3175,80 @@ public class Main {
         path[row][col] = 0;
     }
 } */
+
+
+
+// N Queens Problem
+// Time complexitiy -> O(n!)
+
+
+/*
+public class Main {
+    public static void main(String[] args) {
+        int n = 1;
+        int[][] board = new int[n][n];
+        System.out.println(queens(board, 0));
+    }
+
+    static int queens(int[][] board, int row) {
+        if (row == board.length) {
+            display(board);
+            System.out.println();
+            return 1;
+        }
+
+        int count = 0;
+        
+        for (int col = 0; col < board.length; col++) {
+            if (isSafe(board, row, col)) {
+                board[row][col] = 1;
+                count += queens(board, row + 1);
+                board[row][col] = 0;
+            }
+        }
+        return count;
+    }
+
+    private static boolean isSafe(int[][] board, int row, int col) {
+        // Check vertical row
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 1) {
+                return false;
+            }
+        }
+
+        // Diagonal left
+        int maxLeft = Math.min(row, col);
+        for (int i = 1; i <= maxLeft; i++) {
+            if (board[row - i][col - i] == 1) {
+                return false;
+            }
+        }
+
+        // Diagonal Right
+        int maxRight = Math.min(row, board.length - col - 1);
+        for (int i = 1; i <= maxRight; i++) {
+            if (board[row - i][col + i] == 1) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static void display(int[][] board) {
+        for (int[] row : board) {
+            for (int element : row) {
+                if (element == 1) {
+                    System.out.print("Q ");
+                } else {
+                    System.out.print("X ");
+                }
+            }
+            System.out.println();
+        }
+    }
+} */
  
 
 
