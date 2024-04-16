c4bd9e5cce7b22ddfea3b337dd0a8adc9ecdec09
Minobic <mayank04varma@gmail.com>
2024-04-16 20:03:46 +0530

    N Knights problem added - recursion

--- Main.java
+++ Main.java
@@ -3249,6 +3249,92 @@ public class Main {
         }
     }
 } */
+
+
+
+// N Knights problem
+
+/*
+public class Main {
+    public static void main(String[] args) {
+        int n = 4;
+        int[][] board = new int[n][n];
+        knight(board, 0, 0, n);
+    }
+
+    static void knight(int[][] board, int row, int col, int knights) {
+        if (knights == 0) {
+            display(board);
+            System.out.println();
+            return;
+        }
+
+        if (row == board.length - 1 && col == board.length - 1) {
+            return;
+        }
+
+        if (col == board.length) {
+            knight(board, row + 1, 0, knights);
+            return;
+        }
+
+        if (isSafe(board, row, col)) {
+            board[row][col] = 1;
+            knight(board, row, col + 1, knights - 1);
+            board[row][col] = 0;
+        }
+
+        knight(board, row, col + 1, knights);
+    }
+
+    private static boolean isSafe(int[][] board, int row, int col) {
+        if (isValid(board, row - 2, col - 1)) {
+            if (board[row - 2][col - 1] == 1) {
+                return false;
+            }
+        }
+
+        if (isValid(board, row - 1, col - 2)) {
+            if (board[row - 1][col - 2] == 1) {
+                return false;
+            }
+        }
+
+        if (isValid(board, row - 2, col + 1)) {
+            if (board[row - 2][col + 1] == 1) {
+                return false;
+            }
+        }
+
+        if (isValid(board, row - 1, col + 2)) {
+            if (board[row - 1][col + 2] == 1) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    static boolean isValid(int[][] board, int row, int col) {
+        if (row >= 0 && row < board.length && col >= 0 && col < board.length) {
+            return true;
+        }
+        return false;
+    }
+
+    private static void display(int[][] board) {
+        for (int[] row : board) {
+            for (int element : row) {
+                if (element == 1) {
+                    System.out.print("K ");
+                } else {
+                    System.out.print("X ");
+                }
+            }
+            System.out.println();
+        }
+    }
+} */
  
 
 
