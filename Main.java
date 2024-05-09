ee8af26baacfc42d343d5d80e6efc2b31ab156e9
Minobic <mayank04varma@gmail.com>
2024-05-09 22:27:39 +0530

    Question added - linked list

--- Main.java
+++ Main.java
@@ -5509,15 +5509,264 @@ public class Main {
 
 
 
-// Check wether cycle is present or not
+// Check wether cycle is present or not & also find length of cycle & also find it is starting from
+// Time Complexity -> O(n)
+
+ 
+/*
+class LL {
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+
+        public Node(int value, Node next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public LL() {
+        this.size = 0;
+    }
+
+    public void insertFirst(int val) {
+        Node node = new Node(val);
+        node.next = head;
+        head = node;
+
+        if (tail == null) {
+            tail = head;
+        }
+        size += 1;
+    }
+
+    public void insertLast(int val) {
+        if (tail == null) {
+            insertFirst(val);
+            return;
+        }
+        Node node = new Node(val);
+        tail.next = node;
+        tail = node;
+        size++;
+    }
+
+    public void insert(int val, int index) {
+        if (index == 0) {
+            insertFirst(val);
+            return;
+        }
+        if (index == size) {
+            insertLast(val);
+            return;
+        }
+
+        Node temp = head;
+        for (int i = 1; i < index; i++) {
+            temp = temp.next;
+        }
+
+        Node node = new Node(val, temp.next);
+        temp.next = node;
+
+        size++;
+    }
+
+    public int deleteFirst() {
+        int val = head.value;
+        head = head.next;
+        if (head == null) {
+            tail = null;
+        }
+        size--;
+        return val;
+    }
+
+    public int deleteLast() {
+        if (size <= 1) {
+            return deleteFirst();
+        }
+
+        Node secondLast = get(size - 2);
+        int val = tail.value;
+        tail = secondLast;
+        tail.next = null;
+        size--;
+        return val;
+    }
+
+    public int delete(int index) {
+        if (index == 0) {
+            return deleteFirst();
+        }
+        if (index == size - 1) {
+            return deleteLast();
+        }
+
+        Node prev = get(index - 1);
+        int val = prev.next.value;
+
+        prev.next = prev.next.next;
+        size--;
+        return val;
+    }
+
+    public Node find(int value) {
+        Node node = head;
+        while (node != null) {
+            if (node.value == value) {
+                return node;
+            }
+            node = node.next;
+        }
+        return null;
+    }
+
+    public Node get(int index) {
+        Node node = head;
+        for (int i = 0; i < index; i++) {
+            node = node.next;
+        }
+        return node;
+    }
+
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.value + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("END");
+    }
+
+    public boolean hasCycle() {
+        return helper(head);
+    }
+
+    public boolean helper(Node head) {
+        Node fast = head;
+        Node slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast =  fast.next.next;
+            slow = slow.next;
+
+            if (fast == slow) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public int lengthOfCycle() {
+        return helper2(head);
+    }
+
+    public int helper2(Node head) {
+        Node fast = head;
+        Node slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast =  fast.next.next;
+            slow = slow.next;
+
+            if (fast == slow) {
+                int length = 0;
+                do {
+                    slow = slow.next;
+                    length++;
+                    if (slow == fast) {
+                        return length;
+                    }
+                } while (fast != slow);
+            }
+        }
+
+        return -1;
+    }
+
+    public int cycleStart() {
+        return helper3(head);
+    }
+
+    public int helper3(Node head) {
+        int length = 0;
+        Node fast = head;
+        Node slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast =  fast.next.next;
+            slow = slow.next;
+
+            if (fast == slow) {
+                length = helper2(slow);     // Get length of cycle
+                break;
+            }
+        }
+
+        if (length == 0) {
+            return -1;
+        }
+
+        Node f = head;
+        Node s = head;
+
+        while (length > 0) {
+            s = s.next;
+            length--;
+        }
+
+        while (f != s) {      // Keep moving forward & they will meet at cycle start  
+            f = f.next;
+            s = s.next;
+        }
+
+        return s.value;
+    }
+    
+    public void cycle() {      // Makes a cycle for demo
+        Node node = tail;
+        node.next = head.next.next;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        LL list = new LL();
+
+        list.insertFirst(5);
+        list.insertFirst(7);
+        list.insertFirst(6);
+        list.insertLast(1);
+        list.insertLast(3);
+        list.insertLast(1);
+        
+        list.display();
+        System.out.println(list.hasCycle());
+
+        list.cycle();       // Creates a cycle
+        System.out.println(list.hasCycle());
+        System.out.println(list.lengthOfCycle());
+        System.out.println(list.cycleStart());
+    }
+} */
 
 
 
-// public class Main {
-//     public static void main(String[] args) {
-            // Continue from here
-//     }
-// }
+public class Main {
+    public static void main(String[] args) {
+            
+    }
+}
 /***************************/
 
 
