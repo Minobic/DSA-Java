1cb781804cdd1838c873fa97c690381cf19dd0df
Minobic <mayank04varma@gmail.com>
2024-09-18 18:58:52 +0530

    Karp Raboing Algorithm added

--- Main.java
+++ Main.java
@@ -9957,6 +9957,64 @@ public class Main {
 
 
 
+// Karp Rabin Algorithm     // Used to search ssebset in string
+
+// Strng a = mayank
+// String b = hello i'm mayankraj
+// Time complexity -> O(length(a) + (length(b) * cost of hash fumction))
+
+
+/*
+class KarpRabin {
+    private final int PRIME = 101;
+
+    private double calulateHash(String str) {
+        double hash = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            hash = hash + str.charAt(i) * Math.pow(PRIME, i);
+        }
+        return hash;
+    }
+
+    private double updateHash(double prevHash, char oldChar, char newChar, int patternLength) {
+        double newHash = (prevHash - oldChar) / PRIME;
+        newHash = newHash + newChar * Math.pow(PRIME, patternLength - 1);
+        return newHash;
+    }
+
+    public void search(String text, String pattern) {
+        int patternLength = pattern.length();
+        double patternHash = calulateHash(pattern);
+        double textHash = calulateHash(text.substring(0, patternLength));
+
+        for (int i = 0; i <= text.length() - patternLength; i++) {
+            if (textHash == patternHash) {
+                if (text.substring(i, i + patternLength).equals(pattern)) {
+                    System.out.println("Pattern found at index " + i);
+                }
+            }
+
+            if (i < text.length() - patternLength) {
+                textHash = updateHash(textHash, text.charAt(i), text.charAt(i + patternLength), patternLength);
+            }
+        }
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        KarpRabin algo = new KarpRabin();
+
+        String a = "mayank";
+        String b = "hello i'm mayankraj";
+
+        algo.search(b, a);
+    }
+} */
+
+
+
 /**** Chapter 12 ****/
 /**** File Handling ****/
 
