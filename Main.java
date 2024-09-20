e154c63917982ccbe8efa98d757e3303253956e2
Minobic <mayank04varma@gmail.com>
2024-09-20 11:03:25 +0530

    Count & Radix Sort added

--- Main.java
+++ Main.java
@@ -1442,7 +1442,7 @@ public class Main {
 // Its a stable algorithm
 
 
-/* 
+/*
 public class Main {
     public static void radixSort(int[] arr) {
         int max = Arrays.stream(arr).max().getAsInt();
@@ -1461,14 +1461,20 @@ public class Main {
             count[(arr[i] / exp) % 10]++;
         }
 
+        System.out.println("\nCount array for " + exp + " : " + Arrays.toString(count));
+
         for (int i = 1; i < 10; i++) {
             count[i] = count[i] + count[i - 1];
         }
+
+        System.out.println("Updated count array " + Arrays.toString(count));
         
         for (int i = n - 1; i >= 0; i--) {
             output[count[(arr[i] / exp) % 10] - 1] = arr[i];
             count[(arr[i] / exp) % 10]--; 
         }
+
+        System.out.println("Output array " + Arrays.toString(output));
 
         System.arraycopy(output, 0, arr, 0, n);
     }
