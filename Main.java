136b17899402d67820e7db263b4b85a582a70f72
Minobic <mayank04varma@gmail.com>
2024-09-23 10:24:18 +0530

    Huffman Coding added - Chapter 13

--- Main.java
+++ Main.java
@@ -1063,9 +1063,6 @@ public class Main {
 
 
 /**** Chapter 2 ****/
-
-import java.util.Arrays;
-
 /**** Sorting ****/
 
 
@@ -10366,6 +10363,141 @@ public class Main {
             }
         } catch (IOException e) {
             System.out.println(e.getMessage());
+        }
+    }
+} */
+
+
+
+/**** Chapter 13 ****/
+/**** Huffman Coding ****/
+
+
+// Huffman Coding -> It is a lossless data compression algorithm
+// Time complexity -> O()
+// Space complexity -> O(n)
+
+
+/*
+import java.util.HashMap;
+import java.util.PriorityQueue;
+import java.util.Map;
+import java.util.Set;
+
+class HuffmanCoder {
+    HashMap<Character, String> encoder;
+    HashMap<String, Character> decoder;
+
+    private class Node implements Comparable<Node> {
+        char data;
+        int cost;       // Frequency
+        Node left;
+        Node right;
+
+        public Node(char data, int cost) {
+            this.data = data;
+            this.cost = cost;
+            this.left = null;
+            this.right = null;
+        }
+
+        @Override
+        public int compareTo(Node other) {
+            return this.cost - other.cost;
+        }
+    }
+
+    public HuffmanCoder(String feeder) throws Exception {
+        HashMap<Character, Integer> freqMap = new HashMap<>();
+
+        for (int i = 0; i < feeder.length(); i++) {
+            char ch = feeder.charAt(i);
+
+            if (freqMap.containsKey(ch)) {
+                int freq = freqMap.get(ch);
+                freq += 1;
+                freqMap.put(ch, freq);
+            } else {
+                freqMap.put(ch, 1);
+            }
+        }
+
+        PriorityQueue<Node> minHeap = new PriorityQueue<>();
+        Set<Map.Entry<Character, Integer>> entrySet = freqMap.entrySet();
+
+        for (Map.Entry<Character, Integer> entry : entrySet) {
+            Node node = new Node(entry.getKey(), entry.getValue());
+            minHeap.add(node);
+        }
+
+        while (minHeap.size() != 1) {
+            Node first = minHeap.remove();
+            Node second = minHeap.remove();
+            Node newNode = new Node('\0', first.cost + second.cost);
+            newNode.left = first;
+            newNode.right = second;
+            minHeap.add(newNode);
+        }
+
+        Node fullTree = minHeap.remove();
+
+        this.encoder = new HashMap<>();
+        this.decoder = new HashMap<>();
+
+        this.initEncoderDecoder(fullTree, "");
+    }
+
+    private void initEncoderDecoder(Node node, String outputSF) {
+        if (node == null) {
+            return;
+        }
+
+        if (node.left == null && node.right == null) {
+            this.encoder.put(node.data, outputSF);
+            this.decoder.put(outputSF, node.data);
+        }
+
+        initEncoderDecoder(node.left, outputSF + "0");
+        initEncoderDecoder(node.right, outputSF + "1");
+    }
+
+    public String encode(String source) {
+        String ans = "";
+
+        for (int i = 0; i < source.length(); i++) {
+            ans = ans + encoder.get(source.charAt(i));
+        }
+        return ans;
+    }
+
+    public String decode(String codedString) {
+        String key = "";
+        String ans = "";
+
+        for (int i = 0; i < codedString.length(); i++) {
+            key = key + codedString.charAt(i);
+
+            if (decoder.containsKey(key)) {
+                ans = ans + decoder.get(key);
+                key = "";
+            }
+        }
+        return ans;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        String str = "abbccda";
+        HuffmanCoder hf;
+        try {
+            hf = new HuffmanCoder(str);
+            String cs = hf.encode(str);
+            System.out.println(cs);
+            String dc = hf.decode(cs);
+            System.out.println(dc);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 } */
