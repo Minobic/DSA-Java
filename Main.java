25d3dda6ca00623066211354a1fe2e414d4ebb17
Minobic <mayank04varma@gmail.com>
2024-09-24 11:06:18 +0530

    Sqrt Decomposition / Mo's Algorithm added

--- Main.java
+++ Main.java
@@ -10501,3 +10501,62 @@ public class Main {
         }
     }
 } */
+
+
+
+
+// Sqrt Decomposition / Mo's Algorithm -> It is a technique to solve range queries
+// Time complexity -> O(n * sqrt(n)) for preprocessing and O(q * sqrt(n)) for queries
+// Space complexity -> O(n)
+
+
+/*
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 5, 2, 7, 6, 3, 1, 4, 8};
+        int n = arr.length;
+
+        int sqrt = (int) Math.sqrt(n);
+        int blocks_id = -1;
+        int[] blocks = new int[sqrt + 1];       // Build a blocks array
+
+        for (int i = 0; i < n; i++) {
+            if (i % sqrt == 0) {        // New block is starting
+                blocks_id++;
+            }
+
+            blocks[blocks_id] += arr[i];
+        }
+
+        System.out.println(Arrays.toString(blocks));
+        System.out.println(query(blocks, arr, 2, 7, sqrt));
+    }
+
+    public static int query(int[] blocks, int[] arr, int l, int r, int sqrt) {
+        int ans = 0;
+
+        while (l % sqrt != 0 && l < r && l != 0) {      // Left part
+            ans += arr[l];
+            l++;
+        }
+
+        while (l + sqrt <= r) {     // Middle part
+            ans += blocks[l / sqrt];
+            l += sqrt;
+        }
+
+        while (l <= r) {        // Right part
+            ans += arr[l];
+            l++;
+        }
+        return ans;
+    }
+
+    public void update(int[] blocks, int[] arr, int i, int val, int sqrt) {
+        int block_id = i / sqrt;
+        blocks[block_id] += (val - arr[i]);
+        arr[i] = val;
+    }
+} */
